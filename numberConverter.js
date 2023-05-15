69351eac018618456ffbb023d55746bbd6b9d0dd
Emil <emil@rossing.org>
2023-05-15 15:31:07 +0200

    Updated number converter

--- numberConverter.js
+++ numberConverter.js
@@ -6,17 +6,32 @@
 // Max with 4 chars: 456.976
 // Max with 5 chars: 11.881.376
 class NumberConverter {
-    #allowedChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
+    #allowedChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'];
     #replacementValues = ['Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     #radix = this.#allowedChars.length + this.#replacementValues.length;
     #transpose = (intVal) => this.#replacementValues[intVal];
     constructor() {
         console.debug(`Initialized base ${this.#radix} number converter.`);
     }
-    Convert = (intX) => {                
+    Convert = (intX) => {
         return [...intX.toString(this.#radix)].map(c => {
-            var base10 = parseInt(c, 10);
-            return isNaN(base10) ? c.toUpperCase() : this.#transpose(base10);
+            const base10 = parseInt(c, 10);
+            const converted = isNaN(base10) ? c.toUpperCase() : this.#transpose(base10);
+            const checksum = '';
+            return `${converted}${checksum}`;
         }).join('');
     }
+    ConvertWithChecksum = (intX) => {
+        return this.Convert(intX) + this.Convert(intX % 26);
+    }
+    Unconvert = (strCode) => {
+        // Return the integer (decimal) corresponding to strCode
+    }
+    UnconvertWithChecksum = (strCodeWithCheckum) => {
+        // Return 'false' if code is invalid, otherwise return decimal value of strCodeWithChecksum[..-1]
+        // or return a tuple with (error : true, value = NaN) or (error : false, value = decimal value)
+    }
 }
+
+const x = new NumberConverter();
+console.log(x.getblab());
